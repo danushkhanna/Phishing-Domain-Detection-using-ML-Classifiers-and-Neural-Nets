83687b825e07918ba20295daa16a76cd97cf3d70
Danush Khanna <113158564+danushkhanna@users.noreply.github.com>
2023-05-30 00:46:20 -0700

    Update 🎣🌐_Let's_Go_Phising!.py

--- pages/🎣🌐_Let's_Go_Phising!.py
+++ pages/🎣🌐_Let's_Go_Phising!.py
@@ -25,5 +25,19 @@ height=700
 image_new=image.resize((width,height))
 st.image(image_new)
 
+st.markdown("### *Yep, Literally!*")
+st.markdown("#### *Try It Out!*")
+st.markdown("Type in that link that your 'bank' sent you XD!")
+
+st.markdown("### *Our Approach*")
+st.markdown("To tackle this challenge, we leveraged classical machine learning techniques, including Data Exploration, Data Cleaning, Feature Engineering, Model Building, and Model Testing. Our comprehensive approach involved experimenting with different machine learning algorithms to identify the most suitable ones for this particular case.")
+st.markdown("### *Key Features*")
+st.markdown("- URL-Based Features: We extracted insightful features from the URL itself to capture potential indicators of phishing behavior.")
+st.markdown("- Domain-Based Features: By analyzing the domain properties, we uncovered valuable attributes that help distinguish between genuine and malicious domains.")
+st.markdown("- Page-Based Features: We delved into the contents of web pages associated with each domain, uncovering unique features that shed light on their legitimacy.")
+st.markdown("- Content-Based Features: Leveraging the textual content present on web pages, we derived additional features that contribute to the overall detection accuracy.")
+
+st.markdown("### *Results*")
+st.markdown("Our solution provides a robust and reliable method for predicting whether a domain is authentic or fake. By utilizing the power of machine learning, we have created a model that can effectively discern the telltale signs of phishing attempts, enabling users to make informed decisions and avoid potential security breaches.")
 
 
