b94daa4469e43cab9f50c8f3f5fb4f31159a7f43
Danush Khanna <113158564+danushkhanna@users.noreply.github.com>
2023-05-30 00:47:19 -0700

    Update 🎣🌐_Let's_Go_Phising!.py

--- pages/🎣🌐_Let's_Go_Phising!.py
+++ pages/🎣🌐_Let's_Go_Phising!.py
@@ -29,6 +29,61 @@ st.markdown("### *Yep, Literally!*")
 st.markdown("#### *Try It Out!*")
 st.markdown("Type in that link that your 'bank' sent you XD!")
 
+@st.cache_resource
+def get_model():
+    """
+    Loads the phishing URL detection model from a pickle file.
+
+    This function reads and loads a pickled file containing the classifier.
+
+    Returns:
+        object: The loaded phishing URL detection model.
+
+    Note:
+        The model should be saved in a file named 'phishing_url_detector.pkl'.
+        XGBoost module must be installed before using the file.
+    """
+    with open('phishing_url_detector.pkl', 'rb') as pickle_model:
+        phishing_url_detector = pickle.load(pickle_model)
+    return phishing_url_detector
+
+st.header("Are you sure your 'bank' sent that link?")
+
+# Takes in user input
+input_url = st.text_area("Put in your sus site link here: ")
+
+if input_url != "":
+    
+    # Extracts features from the URL and converts it into a dataframe
+    features_url = ExtractFeatures().url_to_features(url=input_url)
+    features_dataframe = pd.DataFrame.from_dict([features_url])
+    features_dataframe = features_dataframe.fillna(-1)
+    features_dataframe = features_dataframe.astype(int)
+
+    st.write("Okay!")
+    st.cache_data.clear()
+    prediction_str = ""
+
+    # Predict outcome using extracted features
+    try: 
+        phishing_url_detector = get_model()
+        prediction = phishing_url_detector.predict(features_dataframe)
+        if prediction == int(True):
+            prediction_str = 'Phishing Website. Do not click!'
+        elif prediction == int(False):
+            prediction_str = 'Not Phishing Website, stay safe!'
+        else:
+            prediction_str = ''
+        st.write(prediction_str)
+        st.write(features_dataframe)
+
+    except Exception as e:
+        print(e)
+        st.error("Not sure, what went wrong. We'll get back to you shortly!")
+
+else:
+    st.write("")
+
 st.markdown("### *Our Approach*")
 st.markdown("To tackle this challenge, we leveraged classical machine learning techniques, including Data Exploration, Data Cleaning, Feature Engineering, Model Building, and Model Testing. Our comprehensive approach involved experimenting with different machine learning algorithms to identify the most suitable ones for this particular case.")
 st.markdown("### *Key Features*")
